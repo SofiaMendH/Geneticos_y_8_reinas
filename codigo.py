7318c511ace05fe159fa19a83b4929e1498d5c3a
SofiaMendH <97262885+SofiaMendH@users.noreply.github.com>
2023-10-15 00:13:35 +0000

    Se han reducido 4 lineas de código en la  función fitness;  se quitaron las lineas vacías en la función selección;  se cambió nuevamente la función cruza debido a que, de acuerdo a los valores aleatorios tomados,  solía macar un error de 'index out of range', con ello se han logrado reducir 6 líneas; además, se crearon las funciones de mutación (permutación-ordenada) y reemplazo (brecha generacional).  Finalmente se generó la sintaxis del algoritmo genético que nos da como resultado la representación de la matriz cuyo fitness es el mejor (generalmente da 1 y 0).

--- codigo.py
+++ codigo.py
@@ -4,43 +4,34 @@ n=int(input('Ingrese la cantidad de individuos de la población: '))
 
 #Funciones
 
-
 def inicializacion(n):
     Pob=[random.sample(range(8),8) for i in range(n)]
     return Pob
 
 def fitness(Individuo):
-    i=0
     fitness=0
-    while i<7:
-        k=0
-        while k<7:
-            if (k+i+1==8):
-                k=7
+    for i in range(8):
+        for j in range(8):
+            if (j+i+1==8):
+                break
             else:
-                if abs(Individuo[i]-Individuo[k+i+1])==abs(k+i+1-i):
-                    fitness=fitness+1
-                k=k+1
-        i=i+1
+                if abs(Individuo[i]-Individuo[j+i+1])==abs(j+i+1-i):
+                        fitness=fitness+1
     return fitness
 
 def seleccion(Pob):
     suma_fitness=0
     for i in range(n):
         suma_fitness = suma_fitness + all_fitness[i]
-
-
     ind_prob=[all_fitness[i]/suma_fitness for i in range(n)]
-
     acum_prob_num=0
     acum_prob_lista=[]
     for i in range(n):
         acum_prob_num=acum_prob_num + ind_prob[i]
         acum_prob_lista.append(acum_prob_num)
-
-
-    acum_prob=[acum_prob_lista[i] if i==0 else acum_prob_lista[i-1]+acum_prob_lista[i] for i in range(n)]
-
+    acum_prob=[acum_prob_lista[i] if i==0 else 
+               acum_prob_lista[i-1]+acum_prob_lista[i] 
+               for i in range(n)]
     lamda=n/2
     current_member=1
     i=1
@@ -58,38 +49,87 @@ def cruza(Papa,Mama):
     punto_c1=random.randrange(8)
     punto_c2=random.randrange(8)
     if punto_c1 > punto_c2:
-        punto_c1=abs(punto_c2-punto_c1+1)
+        punto_c1=abs(punto_c2-punto_c1+1) 
     Hijo=[]
     for i in range(punto_c1,punto_c2+1):
         Hijo.insert(i,Papa[i])
-    k=0
+    contador=0
+    contador2=0
     for i in range(punto_c2+1,8):
         if Hijo.count(Mama[i])==0:
-            Hijo.insert(i,Mama[i])
-            k=k+1
-    if punto_c2+k<7:
-        i=punto_c2+k+1
-        for j in range(punto_c2):
-            while i<=8:
-                if Hijo.count(Mama[j])==0:
-                    Hijo.insert(i,Mama[j])
-                    i=i+1
-    if len(Hijo)<8:
-        Hijo_parteinicial=[]
-        for i in range(9):
-            if Hijo.count(Mama[i])==0:
-                Hijo_parteinicial.append(Mama[i])
-        Hijo_parteinicial.extend(Hijo)
-        Hijo=Hijo_parteinicial
+            Hijo.insert(punto_c2+1+contador,Mama[i])
+            contador=contador+1
+    for i in range(punto_c2+1):
+        if Hijo.count(Mama[i])==0:
+            if punto_c2 + contador < 7:
+                Hijo.insert(punto_c2+1+contador,Mama[i])
+                contador=contador+1
+            else:
+                Hijo.insert(contador2,Mama[i])
+                contador2=contador2+1
+    return Hijo       
 
-    return Hijo        
+def mutacion(Ind):
+    punto_c1=random.randrange(8)
+    punto_c2=random.randrange(8)
+    Ind_mut=Ind.copy()
+    if punto_c1 > punto_c2:
+        punto=punto_c1
+        punto_c1=punto_c2
+        punto_c2=punto
+    Index=random.sample(range(punto_c1,punto_c2+1), abs(punto_c2-punto_c1+1))
+    k=0
+    for i in range(punto_c1,punto_c2+1):
+        Ind_mut[i]=Ind[Index[k]]
+        k=k+1
+    return Ind_mut
 
+def reemplazo(Pob):
+    i=max(all_fitness)
+    Peores_ind=[]
+    while i>= min(all_fitness):
+        for j in Pob:
+            if fitness(j)==i:
+                Peores_ind.append(j)
+        i=i-1
+    k=random.randrange(25)
+    for i in range(k+1):
+        Peores_ind[i]=Hijos[i]
+    return Peores_ind  
 
-#Código
+#  Código fuente
 
+#Inicialización de la poblacion
 Pob=inicializacion(n)
-
 all_fitness=[fitness(Pob[i]) for i in range(n)]
+print('\n', 'Sus fitness son:', '\n', all_fitness)
 
-Papa=seleccion(Pob)
-Mama=seleccion(Pob)
+for generation in range(20):
+    print('\n', 'GENERACION ', generation)
+
+    #Seleccion de padres
+    papa=seleccion(Pob)
+    mama=seleccion(Pob)
+    mama.reverse()
+
+    #Generacion de hijos por cruza
+    Hijos=[cruza(papa[i], mama[i]) for i in range(25)]
+
+    #Mutacion de los hijos
+    Hijos=[mutacion(Hijos[i]) for i in range(25)]
+
+    #Brecha generacional
+    Pob=reemplazo(Pob)
+
+    #Nuevo fitness
+    all_fitness=[fitness(Pob[i]) for i in range(n)]
+    print('\n', 'Sus fitness son:', '\n', all_fitness)
+
+Mejores_ind=[]
+for i in Pob:
+    if fitness(i)==min(all_fitness):
+        Mejores_ind.append(i)
+
+print('\n', 'Los mejores individuos son: ')
+for i in range(len(Mejores_ind)):
+    print('\n',Mejores_ind[i])
